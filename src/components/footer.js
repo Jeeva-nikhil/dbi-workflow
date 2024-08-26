2c17e46b9ca52a3387ebea95bbc3ab13db5ce0c5
jeeva-arindam <uidesigner@greenjeeva.com>
2024-08-26 12:05:24 +0530

    workflow list page design

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,20 @@
 import React from "react";
-function Footer(){
-    return(
-        <>
+
+function Footer() {
+    return (
         <footer className="footer">
-          <div className="container">
-            <div className="row align-items-center flex-row-reverse">
-              <div className="col-md-12 col-sm-12 text-center">
-                Copyright © <span id="year" />{" "}
-                <a href="#">DIETARY BUSINESS INTELLIGENCE LLC</a>.
-                All rights reserved.
-              </div>
+            <div className="container">
+                <div className="row align-items-center flex-row-reverse">
+                    <div className="col-md-12 col-sm-12 text-center">
+                        Copyright © {new Date().getFullYear()}{" "}
+                        <a href="https://dbi360.com">DIETARY BUSINESS INTELLIGENCE LLC</a>.
+                        All rights reserved.
+                    </div>
+                    
+                </div>
             </div>
-          </div>
         </footer>
-        </>
     );
 }
+
 export default Footer;
